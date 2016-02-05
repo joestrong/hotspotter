97d3b7efe48c32bfee044b1659a2105c427692a3
Joe Strong <joestrong@gmail.com>
2016-02-05 21:59:22 +0000

    Added NodeJS 5.5.0 install to rpi-install.sh

--- rpi-install.sh
+++ rpi-install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 sudo apt-get update
 
-sudo apt-get install -y gpsd gpsd-clients nodejs nodejs-legacy npm
+sudo apt-get install -y gpsd gpsd-clients
+
+# NodeJS 5.5.0
+wget https://nodejs.org/dist/latest-v5.x/node-v5.5.0-linux-armv6l.tar.gz
+tar xzf node-v5.5.0-linux-armv6l.tar.gz 
+rm node-v5.5.0-linux-armv6l.tar.gz
+sudo mv node-v5.5.0-linux-armv6l /opt/nodejs
+sudo ln -s /opt/nodejs/bin/node /usr/local/bin/node
+sudo ln -s /opt/nodejs/bin/npm /usr/local/bin/npm
